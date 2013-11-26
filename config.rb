83343e10bd39b9cf1f4408ff09cab8b8d5725a40
Ainur <nur.php@gmail.com>
2013-11-26 13:57:27 +0700

    end

--- config.rb
+++ config.rb
@@ -1,9 +1,7 @@
 # All methods require authentication (either by client ID or access token).
 # To get your Instagram OAuth credentials, register an app at http://instagr.am/oauth/client/register/
 
-$client_id = ''
-$access_token = ''
+$client_id = 'bd5140a2153044089ea008e166313718'
+$access_token = '33910995.1fb234f.c0fa9d5ebd5f482bab3bc9e4885b2956'
 
-#basic cups options http://www.cups.org/documentation.php/doc-1.7/man-lp.html
-
-$print_params = '-o media=Custom.10x15cm -o position=top-left'
+$images_per_page = 6
